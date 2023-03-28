60b104e7ffddf53624540039501a2a2dbdac6385
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-03-28 14:34:24 -0400

    Three tables done

--- lib/models.py
+++ lib/models.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class Categories(Base):
-    __tablename__ = "Categories"
+class Category(Base):
+    __tablename__ = "categories"
 
     id = Column("Key", Integer, primary_key = True)
     category = Column("category", String)
@@ -14,7 +14,33 @@ class Categories(Base):
         
     def __repr__(self):
         return f"({self.id} {self.category})"
-    
 
-
+class Point (Base):
+    __tablename__ = "points"
     
+    id  = Column("key", Integer, primary_key = True)
+    points = Column("points", Integer)
+
+    def __init__(self, points):
+        self.points = points
+
+    def __repr__(self):
+        return f"({self.id} {self.points})"
+    
+class Question(Base):
+    __tablename__ = "questions"
+
+    id = Column("key", Integer, primary_key = True)
+    question = Column("question", String)
+    answer = Column("answer", String)
+    category = Column(String, ForeignKey("categories.category"))
+    point = Column (Integer, ForeignKey("points.points"))
+
+    def __init__(self, question, answer, category, point):
+        self.question = question
+        self.answer = answer
+        self.category = category
+        self.point = point
+        
+    def __repr__(self):
+        return f"{self.question} {self.answer} {self.category} {self.point}"
