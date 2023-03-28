eb490e76e0585915373ef78df84ff619b61a5b14
Jacob Park <jacobypark122@gmail.com>
2023-03-28 16:54:29 -0400

    edited cli.py

--- lib/cli.py
+++ lib/cli.py
@@ -6,17 +6,39 @@ import random
 if __name__ == '__main__':
     pass
 
+print("\n WELCOME TO JEOPARDY \n")
+
 def pick_category():
-    category = input(' \n Please choose a category')
+    global chosen_cat
+    category = input('Please choose a category: \n')
     if category.lower() == "science":
-        pass
+        pick_point()
     elif category.lower() == "anime":
+        pick_point()
+    elif category.lower() == "bizarre history":
         pass
-    elif category.lower() == "anime":
+    elif category.lower() == "finance":
         pass
-    elif category.lower() == "anime":
-        pass
-    elif category.lower() == "anime":
+    elif category.lower() == "movie quotes":
         pass
     else:
-        print("Please choose a valid category")
+        print("Choose a valid category \n")
+        pick_category()
+
+def pick_point():
+    points = input("For how many points? \n")
+    if points == '100':
+        pass
+    elif points == '200':
+        pass
+    elif points == '300':
+        pass
+    elif points == '400':
+        pass
+    elif points == '500':
+        pass
+    else:
+        print("Choose a valid point value \n")
+        pick_point()
+
+pick_category()
