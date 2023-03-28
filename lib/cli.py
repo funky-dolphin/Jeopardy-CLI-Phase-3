270414b5ef468a1946ecf32dfc8028a7cb2c14a0
Jacob Park <jacobypark122@gmail.com>
2023-03-28 16:27:31 -0400

    edited cli.py

--- lib/cli.py
+++ lib/cli.py
@@ -1,1 +1,22 @@
 
+from models import Category, Point, Question
+import numpy as np
+import random
+
+if __name__ == '__main__':
+    pass
+
+def pick_category():
+    category = input(' \n Please choose a category')
+    if category.lower() == "science":
+        pass
+    elif category.lower() == "anime":
+        pass
+    elif category.lower() == "anime":
+        pass
+    elif category.lower() == "anime":
+        pass
+    elif category.lower() == "anime":
+        pass
+    else:
+        print("Please choose a valid category")
